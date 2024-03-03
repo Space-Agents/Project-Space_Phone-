2acee17b51d1d2b26adde3ff2f842e8b7353040b
Space-Agents <153416891+Space-Agents@users.noreply.github.com>
2024-03-03 20:05:02 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import Phone_body from './components/Phone_body'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Phone_body/>
+     
+    </>
+  )
+}
+
+export default App
